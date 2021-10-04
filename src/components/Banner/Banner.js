916b2f47b2c5fac7f5cf7e48f7f91d884b2fb709
delwarJnu11 <delwar999393@gmail.com>
2021-10-04 21:20:00 +0600

    done and dusted

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Button, Col, Container, Row } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import Header from '../Header/Header';
 import './Banner.css'
 
@@ -15,7 +16,9 @@ const Banner = () => {
                     <Col sm={8} className="text-center text-white banner-text">
                         <h2 className="banner-title">Get Health and well-being with Swimming School.</h2>
                         <p className="banner-description">Swimming is a confusing sport, because sometimes you do it for fun, and other times you do it to not die. And when I'm swimming, sometimes I'm not sure which one it is.</p>
-                        <Button className="fw-bold regular-btn mt-3">Contact Us</Button>
+                        <Link to='/contact'>
+                            <Button className="fw-bold regular-btn mt-3">Contact Us</Button>
+                        </Link>
                     </Col>
                     <Col sm={2}></Col>
                 </Row>
